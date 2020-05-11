42f2c723144aabedfbf5c4b4a1516865fccac4c6
andrewlopezcodes <andrewlopezcodes@gmail.com>
2020-05-11 11:03:14 -0400

    index.js - Required caporal, installed boilerplate for caporal responses, called prog.parse to reply to the user with info to help interact with the program - package.json - Installed caporal.

--- index.js
+++ index.js
@@ -2,13 +2,24 @@
 
 const debounce = require('lodash.debounce');
 const chokidar = require('chokidar');
-
-const start = debounce(() => {
-  console.log('STARTING USERS PROGRAM')
-}, 100);
+const prog = require('caporal');
 
 
-chokidar.watch('.')
-  .on('add', start)
-  .on('change', () => console.log('FILE CHANGED'))
-  .on('unlink', () => console.log('FILE UNLINKED'));
+prog
+  .version('0.0.1')
+  .argument('[filename]', 'Name of file to execute')
+  .action((args) => {
+    console.log(args);
+  });
+
+prog.parse(process.argv);
+
+// const start = debounce(() => {
+//   console.log('STARTING USERS PROGRAM')
+// }, 100);
+
+
+// chokidar.watch('.')
+//   .on('add', start)
+//   .on('change', () => console.log('FILE CHANGED'))
+//   .on('unlink', () => console.log('FILE UNLINKED'));
