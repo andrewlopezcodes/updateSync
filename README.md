00b7667bd31bd440ba8af518385843a649df903a
andrewlopezcodes <andrewlopezcodes@gmail.com>
2020-05-11 11:55:40 -0400

    READEME.md - Updated Node Standard Library link - index.js - Required fs, moved debounce function inside the action method callback function, wrapped callback function in async pattern.

--- README.md
+++ README.md
@@ -8,4 +8,6 @@
 2. Chokidar - https://www.npmjs.com/package/chokidar
 3. Caporal - https://www.npmjs.com/package/caporal
 4. lodash.debounce - https://www.npmjs.com/package/lodash.debounce
-4. Node Standard Library modules
+4. Node Standard Library modules - https://nodejs.org/api/fs.html#fs_fspromises_access_path_mode
+
+
--- index.js
+++ index.js
@@ -3,23 +3,33 @@
 const debounce = require('lodash.debounce');
 const chokidar = require('chokidar');
 const prog = require('caporal');
+const fs = require('fs');
+
 
 
 prog
   .version('0.0.1')
   .argument('[filename]', 'Name of file to execute')
-  .action((args) => {
-    console.log(args);
+  .action(async ({
+    filename
+  }) => {
+    const name = filename || 'index.js';
+    try {
+      await fs.promises.access(name);
+    } catch (err) {
+      throw new Error(`ERROR !!!!!! Could not find the file ${name}`);
+    }
+
+
+    const start = debounce(() => {
+      console.log('STARTING USERS PROGRAM')
+    }, 100);
+
+
+    chokidar.watch('.')
+      .on('add', start)
+      .on('change', () => start)
+      .on('unlink', () => start);
   });
 
 prog.parse(process.argv);
-
-// const start = debounce(() => {
-//   console.log('STARTING USERS PROGRAM')
-// }, 100);
-
-
-// chokidar.watch('.')
-//   .on('add', start)
-//   .on('change', () => console.log('FILE CHANGED'))
-//   .on('unlink', () => console.log('FILE UNLINKED'));
